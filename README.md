05dad5ae39871928ea9e8a1f51dc8813f5900358
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2023-10-10 09:32:36 -0300

    Finalizado curso básico

--- README.md
+++ README.md
@@ -13,3 +13,21 @@ yarn add express
 # rodar servidor
 
 node index ou arquivo de configuração do servidor
+
+# instalar nodemon
+
+yarn add nodemon -D para adicionar como dependencia de desenvolvimento
+
+# Executar servicor com nodemon
+
+npx nodemon arquivo.js de configuração do server.
+
+# criar script no package.json para executar o nodemon via yarn
+
+"scripts": {
+"dev": "nodemon index.js"
+}
+
+# após colocar o script é só executar o comando com yarn para executar o servidor
+
+yarn dev
--- index.js
+++ index.js
@@ -2,12 +2,59 @@ const express = require("express");
 
 const server = express();
 
-server.get("/hello", (req, res) => {
-  const nome = req.query.nome;
-  return res.json({
-    title: "Hello WOrld",
-    message: `Olá ${nome}, tudo bem?`,
-  });
+server.use(express.json());
+
+let customers = [
+  { id: 1, name: "Dev Samurai", site: "http://devsamurai.com.br" },
+  { id: 2, name: "Gooogle", site: "http://google.com" },
+  { id: 3, name: "UOL", site: "http://uol.com.br" },
+];
+
+server.get("/customers", (req, res) => {
+  return res.json(customers);
+});
+
+server.get("/customers/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const customer = customers.find((item) => item.id == id);
+  const status = customer ? 200 : 404;
+
+  return res.status(status).json(customer);
+});
+
+server.post("/customers", (req, res) => {
+  const { name, site } = req.body;
+  const id = customers[customers.length - 1].id + 1;
+  const newCustomer = { id, name, site };
+  customers.push(newCustomer);
+  // console.log(customers);
+
+  return res.status(201).json(customers);
+});
+
+server.put("/customers/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name, site } = req.body;
+
+  const index = customers.findIndex((item) => item.id === id);
+  const status = index >= 0 ? 200 : 404;
+
+  if (index >= 0) {
+    customers[index] = { id: parseInt(id), name, site };
+  }
+
+  return res.status(status).json(customers[index]);
+});
+
+server.delete("/customers/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = customers.findIndex((item) => item.id === id);
+  const status = index >= 0 ? 200 : 404;
+
+  if (index >= 0) {
+    customers.splice(index, 1);
+  }
+  return res.status(status).json();
 });
 
 server.listen(3000);
